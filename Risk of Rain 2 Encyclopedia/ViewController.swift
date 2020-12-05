8f5aba598c4a5081b9146dcb4d396d8e9a1ff1de
Joshua Baldwin <baldwinjd2@appstate.edu>
2020-12-05 16:18:52 -0500

    Initial Commit

---
+++ Risk of Rain 2 Encyclopedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Risk of Rain 2 Encyclopedia
+//
+//  Created by user180257 on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
